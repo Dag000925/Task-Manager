9fb97fd35e17497c9980a04b1073df6a33857309
dag242 <121731315+dag242@users.noreply.github.com>
2023-11-18 11:35:56 -0500

    ignore

--- src/screen/Log.js
+++ src/screen/Log.js
@@ -1,16 +1,63 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, ScrollView } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, ScrollView, TextInput, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
+import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 
+const Log = () =>{
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH; 
 
-export default function Log() {
+  const signIn = async ()=> {
+    setLoading(true);
+    try{
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      alert('Registration Failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const signUp = async () =>{
+    setLoading(true);
+    try{
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert('Check Registered Email');
+    } catch (error){
+      console.log(error);
+      alert('Sign in Failed: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.centeredContent}>
-        <Text style={styles.message}>Log In</Text>
-        <StatusBar style="auto" />
+        <KeyboardAvoidingView behavior="padding">
+        <TextInput value={email} style={styles.input} placeholder='Email' autoCapitalize='none'
+          onChangeText={(text) => setEmail(text)}></TextInput>
+
+        <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder='Password' autoCapitalize='none'
+          onChangeText={(text) => setPassword(text)}></TextInput>
+          <StatusBar style="auto" />
+
+      { loading ? (<ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <> 
+        <Button title="Login" onPress={signIn}/>
+        <Button title="Create Account" onPress={signUp}/>
+        </>
+      )}
+      </KeyboardAvoidingView>
       </View>
     </View>
   );
@@ -32,3 +79,5 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
 });
+
+export default Log;
